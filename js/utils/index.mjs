8f5a10eff4b86cc0cb05e72f08dbb90933a2889f
myjyby <myjyby@gmail.com>
2026-02-13 22:28:45 +0100

    resting the repo

--- js/utils/index.mjs
+++
@@ -1,6 +0,0 @@
-const { origin, pathname } = new URL(window.location);
-const params = pathname.split('/').filter(d => d.length);
-
-export const platform = origin.includes('localhost') ? params[0] : params[1];
-export const baseurl = new URL(`https://${platform}.sdg-innovation-commons.org`);
-export const islocal = new URL(window.location).origin.includes('localhost');
